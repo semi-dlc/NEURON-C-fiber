592153ab41041e2df8ce3582f9e83216133c7c28
Dino Cheng <71894595+semi-dlc@users.noreply.github.com>
2026-03-09 23:57:54 +0100

    Update run.py

--- run.py
+++ run.py
@@ -4,46 +4,73 @@ from multiprocessing import Pool
 
 neuron.load_mechanisms('./MOD_Tigerholm')
 
+
+params_orig = {
+    'gPump': -0.00485891709589456,
+    'gNav17Parent': 0.22831806252579703,
+    'gNav17': 0.33198932295899997,
+    'gNav18Parent': 0.2411879110860178,
+    'gNav18': 0.04520936848183172,
+    'gNav19': 0.0032208034421239012,
+    'gKs': 0.0030962055552702606,
+    'gKf': 0.025994684685553986,
+    'gH': 0.009167424690232623,
+    'gKdr': 0.013875365047132446,
+    'gKna': 0.0014444412024341238,
+    'vRest': -55.0
+}
+
+
+changes = [-0.6, -0.4, -0.2, -0.1, 0.1, 0.2, 0.4, 0.6, 0.8]
+
+changes_v_rest  = [-0.3, -0.2, -0.1, 0.1, 0.2, 0.4]
+
+changes_gNav18  = [-0.6, -0.4, -0.2, -0.1, 0.1, 0.2, 0.3]
+
+params_dict = {}
+
 params = []
 
-gPump = -0.00485891709589456
-gNav17 = 0.33198932295899997
-gNav17Parent=0.22831806252579703
-gNav18 = 0.04520936848183172
-gNav18Parent=0.2411879110860178
-gNav19 = 0.0032208034421239012
-gKs = 0.0030962055552702606
-gKf = 0.025994684685553986
-gH = 0.009167424690232623
-gKdr = 0.013875365047132446
-gKna = 0.0014444412024341238
-v_rest=-55.
+# assign parameter sets
+for name, val in params_orig.items():
+    params_dict[name] = {}
 
-param_orig = [
-    gPump,
-    gNav17Parent,
-    gNav18Parent,
-    gNav19,
-    gKs,
-    gKf,
-    gH,
-    gKdr,
-    gKna
-]
+    if name in ['gNav17Parent', 'gNav18Parent', 'gKf']:
+        continue
+    elif name in ['gNav17', 'gNav18']:
+        parent_name = name + 'Parent'
+        if name == 'gNav18':
+            for dg in changes_gNav18:
+                new_p = params_orig.copy()
+                new_p[name] = params_orig[name] * (1 + dg)
+                new_p[parent_name] = params_orig[parent_name] * (1 + dg)
+                params_dict[name][dg] = new_p
+                params.append(list(new_p.values()))
+        else:
+            for dg in changes:
+                new_p = params_orig.copy()
+                new_p[name] = params_orig[name] * (1 + dg)
+                new_p[parent_name] = params_orig[parent_name] * (1 + dg)
+                params_dict[name][dg] = new_p
+                params.append(list(new_p.values()))
+    elif name == 'vRest':
+        for dg in changes_v_rest:
+            new_p = params_orig.copy()
+            new_p[name] = params_orig[name] * (1 + dg)
+            params_dict[name][dg] = new_p
+            params.append(list(new_p.values()))
+    else:
+        for dg in changes:
+            new_p = params_orig.copy()
+            new_p[name] = params_orig[name] * (1 + dg)
+            params_dict[name][dg] = new_p
+            params.append(list(new_p.values()))
 
-param_names = ['gPump', 'gNav17Parent', 'gNav18Parent', 'gNav19', 'gKs', 'gKf', 'gH', 'gKdr', 'gKna']
-
-changes = [-0.2, -0.15, -0.1, -0.05, 0.0, 0.05, 0.1, 0.15, 0.2, 0.25]
-
-for dg in changes:
-    for i, x in enumerate(param_orig):
-        param_new = param_orig.copy()
-        param_new[i] = param_orig[i] * (1 + dg) # scale 1 parameter
-        params.append(param_new)
+print(f"Generated {len(params)} unique parameter combinations.")
 
 if __name__ == '__main__':
-    # Parallelize the execution
-    with Pool(processes=5) as pool:
+    # Parallelize
+    with Pool(processes=5) as pool: # 6 core cpu -> 5 processes so 1 is empty (hopefully?)
         results = []
         for param in params:
             # Create kwargs dictionary with varied parameters
@@ -53,14 +80,16 @@ if __name__ == '__main__':
                 'scalingFactor': 0.1,
                 'gPump': param[0],
                 'gNav17Parent': param[1],
-                'gNav18Parent': param[2],
-                'gNav19': param[3],
-                'gKs': param[4],
-                'gKf': param[5],
-                'gH': param[6],
-                'gKdr': param[7],
-                'gKna': param[8],
-                'vRest': vRest
+                'gNav17': param[2],
+                'gNav18Parent': param[3],
+                'gNav18': param[4],
+                'gNav19': param[5],
+                'gKs': param[6],
+                'gKf': param[7],
+                'gH': param[8],
+                'gKdr': param[9],
+                'gKna': param[10],
+                'vRest': param[11]
             }
 
             # Submit job to pool with keyword arguments
